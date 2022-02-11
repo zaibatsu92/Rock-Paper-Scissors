d14e3a1559c06912db45169cf63b4ec1fc9f272e
Christian <Christian@CHRISTIANPC>
2022-02-11 16:11:50 +0100

    Readme.md and runnable jar file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Rock-Paper-Scissors
+This program plays a set number of games of rock paper scissors and presents the result of each game and the overall statistics at the end.
+
+Runnable jar is in build/libs
--- build.gradle.kts
+++ build.gradle.kts
@@ -27,3 +27,13 @@ tasks.withType<KotlinCompile> {
 application {
     mainClass.set("MainKt")
 }
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
